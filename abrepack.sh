74b3551389786db10e4d61e7c55f0c39580dcb8b
George Gordeev <ggordeev@rbc.ru>
2019-03-01 18:00:43 +0300

    Initial commit and completely working application

---
+++ abrepack.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+source "$(dirname $0)/abrepackrc"
+
+usage_message="\nUsage: $0 <command> <package/application name>\n
+\tCommands:
+\t\tsearch <string> - search string in package list on device
+\t\tget <app>       - backup app into ab-file
+\t\tunpack          - unpack ab-file (unpack to tar and untar)
+\t\trepack          - pack untared data into ab-file
+\t\textractapp <app> - backup app, unpack to tar-file and untar it
+\t\tclear           - remove all tmp files and dir apps
+"
+
+[[ $# -eq 0 ]] && echo -e "$usage_message" && exit 1
+
+case "$1" in
+    'search')
+        search_app $2
+        ;;
+    'get')
+        backup_app $2
+        ;;
+    'unpack')
+        unpack_ab2tar
+        untar
+        ;;
+    'extractapp')
+        backup_app $2
+        unpack_ab2tar
+        untar
+        ;;
+    'repack')
+        pack_ab
+        ;;
+    'clear')
+        [ -d apps ] && rm -rf apps
+        [ -e $FILESLIST ] && rm $FILESLIST
+        [ -e $HEADERFILE ] && rm $HEADERFILE
+        [ -e $BFILE ] && rm $BFILE
+        [ -e $TFILE ] && rm $TFILE
+        echo 'Cleared'
+        ;;
+    'param2')
+        echo 'param2'
+        ;;
+    *)
+        echo -e "$usage_message"
+        exit 0
+        ;;
+esac
